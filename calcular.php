ab37c188a13b6930f40c39ca80d25fd297b25dad
Gloria <glorimichelle6900@gmail.com>
2021-07-13 19:24:57 -0500

    Modificamos el php

--- calcular.php
+++ calcular.php
@@ -1,4 +1,17 @@
 <?php 
+$edad = 24;
+switch($edad)
+{
+    case 12:
+        echo "La mitad de tu edad";
+        break;
+    case 24:
+        echo "Esta es tu edad";
+        break;
+    case 48:
+        echo "El doble de tu edad";
+        break;
+}
 $cantidad = $_POST['cantidad'];
 $subtotal = $cantidad*125;
 $numero_cajas = ceil($cantidad/4);
